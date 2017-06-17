66ebc24b2996ae8fc05baec3a9445d0592808d2e
William A Sims III <willsims14@gmail.com>
2017-06-17 13:20:43 -0500

    Student profile is now retrieved via token rather username

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -9,62 +9,40 @@ angular.module('MyGrades').controller('ProfileCtrl', [
     '$routeParams',
     function($scope, $http, $location, RootFactory, apiUrl, $routeParams) {
 
-        console.log("API URL (PROFILE): ", apiUrl);
 
         $scope.current_user = $routeParams;
 
-        console.log("User: ", $scope.current_user);
-        console.log("Token: ", RootFactory.getToken());
 
-        //  RootFactory.getApiRoot()
-        // .then( (root) => {
-        //     console.log("Root: ", root);
-        //     $http({
-        //         url: `http://localhost:8000/getstudent/${RootFactory.getToken()}/`,
-        //         headers: {
-        //             "Content-Type": "application/json",
-        //             'Authorization': "Token " + RootFactory.getToken()
-        //         }
-        //     }).then( function(response) {
-        //         console.log("Token Response: ", response);
-        //     });
-
-
-     RootFactory.getApiRoot()
-    .then( (root) => {
-        console.log("Root: ", root);
-        $http({
-            url: `http://localhost:8000/student/detail/${$scope.current_user.username}/`,
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': "Token " + RootFactory.getToken()
-            },
-            params:{
-                'username': $scope.current_user.username
-        }
-        }).then( function(res) {
-            console.log("User Response: ", res.data.results);
-            $scope.student = res.data.results[0];
-
-
+        RootFactory.getApiRoot()
+        .then( (root) => {
+            // Get logged in student's profile
             $http({
-            url: `http://localhost:8000/student-course/`,
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': "Token " + RootFactory.getToken()
-            },
-            params:{
-                'student_id': parseFloat($scope.student.id)
-            }
-            }).then( function(res) {
-                console.log("Courses Response: ", res.data.results);
-                $scope.student.courses = res.data.results;
+                url: `http://localhost:8000/getstudent/${RootFactory.getToken()}/`,
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': "Token " + RootFactory.getToken()
+                }
+            }).then( function(response) {
+                console.log("Student-Token Response: ", response);
+                $scope.student = response.data;
+
+                // Get student's courses
+                $http({
+                url: `http://localhost:8000/student-course/`,
+                headers: {
+                    "Content-Type": "application/json",
+                    'Authorization': "Token " + RootFactory.getToken()
+                },
+                params:{
+                    'student_id': parseFloat($scope.student.id)
+                }
+                }).then( function(res) {
+                    console.log("Courses Response: ", res.data.results);
+                    $scope.student.courses = res.data.results;
+                });
             });
+
         });
-
-    });
-   
-
 }]);
 
 
