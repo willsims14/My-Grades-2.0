d9b0d13b1c4d6349905d3e5b02b5741754c16d0b
William A Sims III <willsims14@gmail.com>
2017-06-17 14:28:18 -0500

    Setup ctrl and partial to create assignment

--- app/controllers/CreateCourseCtrl.js
+++ app/controllers/CreateCourseCtrl.js
@@ -11,30 +11,6 @@ angular.module('MyGrades').controller('CreateCourseCtrl', [
 
         $scope.course = {};
 
-
-
-
-        RootFactory.getApiRoot()
-        .then( (root) => {
-            console.log("Root: ", root);
-            
-
-            // $http({
-            // url: `http://localhost:8000/student-course-assignments/`,
-            // headers: {
-            //     "Content-Type": "application/json",
-            //     'Authorization': "Token " + RootFactory.getToken()
-            // },
-            // params:{
-            //     'course_id': parseFloat($scope.course.id)
-            // }
-            // }).then( function(res) {
-            //     console.log("Assignment Response: ", res.data);
-            //     $scope.assignments = res.data.results;
-            // });
-
-
-        });
        
         $scope.create_course = function(event){
             console.log("Course: ", $scope.course); 
@@ -62,19 +38,10 @@ angular.module('MyGrades').controller('CreateCourseCtrl', [
                         console.log("Response Token: ", res.data);
                         console.log("RootFactory Token: ", user_token);
                         if (res.data.token !== "") {
-                            console.log("SUCCESS!!!!!!!!!!!!!");
+
                             // $location.path(`/profile/${$scope.user.username}`);
                         }
-                    },
-                    console.error
-                );
+                });
             });
-
-
-
-
-
-
-
         };
 }]);
