e80bb0ca5e3991838a7053b7e7f92814c2c88f49
William A Sims III <willsims14@gmail.com>
2017-06-18 13:33:55 -0500

    Creation and deletion of courses occurs in factory rather ctrl

--- app/controllers/CreateCourseCtrl.js
+++ app/controllers/CreateCourseCtrl.js
@@ -7,41 +7,28 @@ angular.module('MyGrades').controller('CreateCourseCtrl', [
     'RootFactory',
     'apiUrl',
     '$routeParams',
-    function($scope, $http, $location, RootFactory, apiUrl, $routeParams) {
+    'CourseFactory',
+    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, CourseFactory) {
 
         $scope.course = {};
 
        
         $scope.create_course = function(event){
-            console.log("Course: ", $scope.course); 
-
-            RootFactory.getApiRoot()
-            .then( (root) => {
-                console.log("Root: ", root);
-
-                $http({
-                    url: `${apiUrl}/create-course/`,
-                    method: "POST",
-                    data: { 
-                        "title": $scope.course.title,
-                        "course_number": $scope.course.course_number,
-                        "professor": $scope.course.professor,
-                    },
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-                        'Authorization': "Token " + RootFactory.getToken()
-                    }
-                }).then(
-                    res => {
-                        console.log("Response!!!: ", res);
-                        var user_token = "Token " + RootFactory.getToken();
-                        console.log("Response Token: ", res.data);
-                        console.log("RootFactory Token: ", user_token);
-                        if (res.data.token !== "") {
-
-                            // $location.path(`/profile/${$scope.user.username}`);
-                        }
-                });
+            CourseFactory.createCourse($scope.course)
+            .then( function(response) {
+                console.log("Create Course Response: ", response);
             });
         };
+
+
+
+
+
+
+
+
+
+
 }]);
+
+
---
+++ app/factories/CourseFactory.js
@@ -0,0 +1,85 @@
+"use strict";
+
+
+
+app.factory("CourseFactory", function(apiUrl, RootFactory, $q, $http){
+
+
+    let getCourse = function(course_id){
+        return $q((resolve, reject) => {
+        RootFactory.getApiRoot()
+        .then( (root) => {
+                $http({
+                    url: `http://localhost:8000/student-course-assignments/`,
+                    headers: {
+                        "Content-Type": "application/json",
+                        'Authorization': "Token " + RootFactory.getToken()
+                    },
+                    params:{
+                        'course_id': parseFloat(course_id)
+                    }
+                })
+                .then((res) => {
+                    resolve(res);
+                }).catch((error) => {
+                    reject(error);
+                });
+            });
+        });
+    };
+
+    let createCourse = function(course){
+        return $q((resolve, reject) => {
+            RootFactory.getApiRoot()
+            .then( (root) => {
+                console.log("Root: ", root);
+
+                $http({
+                    url: `${apiUrl}/create-course/`,
+                    method: "POST",
+                    data: { 
+                        "title": course.title,
+                        "course_number": course.course_number,
+                        "professor": course.professor,
+                    },
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                        'Authorization': "Token " + RootFactory.getToken()
+                    }
+                }).then( function(res){
+                    resolve(res);
+                }).catch( function(error){
+                    reject(error);
+                });
+            });
+        });
+    };
+
+    let deleteCourse = function(course_id){
+        return $q((resolve, reject) => {
+            RootFactory.getApiRoot()
+            .then( (root) => {
+                $http({
+                    url: `http://localhost:8000/student-course-delete/${course_id}/`,
+                    method: 'DELETE',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Token " + RootFactory.getToken()
+                    }
+                }).then( function(response) {
+                    if (response.status === 204){
+                        resolve(response);
+                    }else{ reject(response); }
+                }).catch( function(error) {
+                    reject(error);
+                });
+
+            });
+        });
+    };
+
+
+
+    return { getCourse, deleteCourse, createCourse };
+
+});
