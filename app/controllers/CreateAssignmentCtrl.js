47b077c1cb8381788f17c5c8334d826f6cf39a48
William A Sims III <willsims14@gmail.com>
2017-06-18 13:39:19 -0500

    Setup

--- app/controllers/CreateAssignmentCtrl.js
+++ app/controllers/CreateAssignmentCtrl.js
@@ -7,39 +7,24 @@ angular.module('MyGrades').controller('CreateAssignmentCtrl', [
     'RootFactory',
     'apiUrl',
     '$routeParams',
-    function($scope, $http, $location, RootFactory, apiUrl, $routeParams) {
+    'AssignmentFactory',
+    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, AssignmentFactory) {
 
         $scope.assignment = {};
         $scope.course = $routeParams.course_title;
 
         console.log("CREATE ASSIGNMENT CTRL");
        
-        // $scope.create_assignment = function(event){
-        //     console.log("Assignment: ", $scope.assignment); 
+        $scope.create_assignment = function(event){
+            
 
-        //     RootFactory.getApiRoot()
-        //     .then( (root) => {
-        //         console.log("Root: ", root);
+        };
 
-        //         $http({
-        //             url: `${apiUrl}/create-assignment/`,
-        //             method: "POST",
-        //             data: { 
-        //                 "title": $scope.assignment.
-        //                 "course_number": $scope.assignment.,
-        //                 "professor": $scope.assignment.,
-        //             },
-        //             headers: {
-        //                 "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        //                 'Authorization': "Token " + RootFactory.getToken()
-        //             }
-        //         }).then( function(res) {
-        //                 console.log("New Assignment Response: ", res);
-        //                 var user_token = "Token " + RootFactory.getToken();
-        //                 if (res.data.token !== "") {
-        //                     // $location.path(`/profile/${$scope.user.username}`);
-        //                 }
-        //         });
-        //     });
-        //}
+
+
+
+
+
+
+
 }]);
