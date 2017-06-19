592f5cfd7eb7015d48952d2fc448576fec849f6f
William A Sims III <willsims14@gmail.com>
2017-06-19 15:32:57 -0500

    User can now delete assignments

--- app/controllers/CourseCtrl.js
+++ app/controllers/CourseCtrl.js
@@ -8,7 +8,16 @@ angular.module('MyGrades').controller('CourseCtrl', [
     'apiUrl',
     '$routeParams',
     'CourseFactory',
-    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, CourseFactory) {
+    'AssignmentFactory',
+    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, CourseFactory, AssignmentFactory) {
+
+        $(document).ready(function(){
+            $('.tooltipped').tooltip({delay: 50});
+            $('.tooltipped').tooltip({position: 'top'});
+
+
+        });
+
 
         $scope.course = {};
         $scope.course.title = $routeParams.course_title;
@@ -21,13 +30,25 @@ angular.module('MyGrades').controller('CourseCtrl', [
 
 
 
-
         $scope.deleteCourse = function(course_id) {
             CourseFactory.deleteCourse(course_id)
             .then( function(res) {
                 if(res.status === 204){
-                    console.log("Delete Succesful");
+                    Materialize.toast('Deleted Course', 5000);
                 }else{ console.log("Delete Unsuccesful"); }
+            });
+        };
+
+        $scope.deleteAssignment = function(assignment_id) {
+            AssignmentFactory.deleteAssignment(assignment_id)
+            .then( function(res) {
+                if (res.status === 204){
+                    Materialize.toast('Deleted Assignment', 5000);
+                    CourseFactory.getCourse($scope.course.id)
+                    .then( function(res) {
+                        $scope.assignments = res.data.results;
+                    });
+                }else{ console.log("Assignment Delete Failure"); }
             });
         };
 
