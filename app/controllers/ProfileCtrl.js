ece7238c46ebd06ab2b849e8be0e3b78f05478f0
William A Sims III <willsims14@gmail.com>
2017-06-20 09:40:19 -0500

    Users can filter courses by semester now

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -8,7 +8,21 @@ angular.module('MyGrades').controller('ProfileCtrl', [
     'apiUrl',
     '$routeParams',
     'StudentFactory',
-    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, StudentFactory) {
+    'CourseFactory',
+    function($scope, $http, $location, RootFactory, apiUrl, $routeParams, StudentFactory, CourseFactory) {
+
+        $( document ).ready( function(){
+            $('.dropdown-button').dropdown({
+                inDuration: 300,
+                outDuration: 225,
+                constrainWidth: false, // Does not change width of dropdown to that of the activator
+                hover: true, // Activate on hover
+                gutter: 5, // Spacing from edge
+                belowOrigin: false, // Displays dropdown below the button
+                alignment: 'left', // Displays dropdown with edge aligned to the left of button
+                stopPropagation: false // Stops event propagation
+            });
+        });
 
         $scope.current_user = $routeParams;
 
@@ -23,8 +37,19 @@ angular.module('MyGrades').controller('ProfileCtrl', [
             .then( function(res) {
                 console.log("Courses Response: ", res.data.results);
                 $scope.student.courses = res.data.results;
+                CourseFactory.getSemesters()
+                .then( function(response) {
+                    $scope.semesters = response.data.results;
+                });
             });
         });
+
+
+        $scope.setSemesterFilter = function(selected_semester){
+            $scope.semester_filter = selected_semester;
+            console.log("$scope.semester: ", $scope.semester_filter);
+
+        };
 
 
 
